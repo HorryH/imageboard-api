6ceed21367c1ebf3a22d692cbecac90e5d2e919c
ZHANG CHENG HUANG <zc2huang@uwaterloo.ca>
2018-07-29 12:28:30 -0400

    post ranking

--- helpers/imageboarddb.js
+++ helpers/imageboarddb.js
@@ -1,7 +1,7 @@
 import * as dynamoDbLib from "./dynamodb";
 
 const imageboard = "imageboard-main";
-const ibRank = "imageboard-rank";
+const imageboardRank = "imageboard-rank";
 
 export function getMain(pid, ...args) {
   const params = {
@@ -35,7 +35,7 @@ export function updateMain(pid, updateExpression, updateValues, ...args) {
 
 export function createRank(item, ...args) {
   const params = {
-    TableName: ibRank,
+    TableName: imageboardRank,
     Item: item
   };
   return dynamoDbLib.handledPut(params, ...args);
@@ -43,7 +43,7 @@ export function createRank(item, ...args) {
 
 export function getRanks(exclusiveStartKey, ...args) {
   const params = {
-    TableName: ibRank,
+    TableName: imageboardRank,
     AttributesToGet: [
       "pid"
     ],
@@ -52,4 +52,16 @@ export function getRanks(exclusiveStartKey, ...args) {
   if (exclusiveStartKey)
     params.ExclusiveStartKey = exclusiveStartKey;
   return dynamoDbLib.handledScan(params, ...args)
+}
+
+export function updateRank(pid, updateExpression, updateValues, ...args) {
+  const params = {
+    TableName: imageboardRank,
+    Key: {
+      pid: pid
+    },
+    UpdateExpression: updateExpression,
+    ExpressionAttributeValues: updateValues
+  };
+  return dynamoDbLib.handledUpdate(params, ...args);
 }
