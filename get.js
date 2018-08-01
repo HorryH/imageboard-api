32023de8ef355373d52a4531b9d846104337d42e
ZHANG CHENG HUANG <zc2huang@uwaterloo.ca>
2018-07-31 22:34:17 -0400

    changed id to pid

--- get.js
+++ get.js
@@ -2,7 +2,7 @@ import * as imageboardDB from "./helpers/imageboarddb";
 import { successOrNull } from "./helpers/response";
 
 export async function main(event, context, callback) {
-  const pid = event.pathParameters.id;
+  const pid = event.pathParameters.pid;
   const results = await imageboardDB.getMain(pid, context, callback);
   callback(null, successOrNull(results));
 }
