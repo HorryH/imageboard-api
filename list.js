486c4e7188558672a56ba88525aa5f8a82986736
Harry Huang <sam.harry.huang@gmail.com>
2018-08-20 22:57:50 -0400

    quick null check

--- list.js
+++ list.js
@@ -2,7 +2,7 @@ import * as imageboardDB from "./helpers/imageboarddb";
 import { successOrNull } from "./helpers/response";
 
 export async function main(event, context, callback) {
-  const data = JSON.parse(event.body);
+  const data = JSON.parse(event.body) || {};
   const results = await imageboardDB.listByScore(50, data.startKey, callback);
   const postList = await imageboardDB.batchGetMain(results.Items, context, callback);
   postList.list = results.Items;
